6f19a7156388f85032e3aa48eb562c0d0511b374
David Garcia <david.a.g.r93@gmail.com>
2018-09-15 16:51:21 -0400

    Break SRP

--- Examples/SRP/src/SRP.java
+++ Examples/SRP/src/SRP.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,14 +22,19 @@ class Journal {
         return String.join(System.lineSeparator(), entries);
     }
 
-    public void save(String filename) {
-        
+    public void save(String filename) throws FileNotFoundException {
+        try (PrintStream out = new PrintStream(filename)) {
+            out.println(toString());
+        }
     }
+
+    public void load(String filename) {}
+    public void load(URL url){}
 
 }
 
 class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Journal j = new Journal();
         j.addEntry("I cried today");
         j.addEntry("I ate a bug");
