aafe3e20b05a851e7d9c18174bc2259d12557448
David Garcia <david.a.g.r93@gmail.com>
2018-09-15 22:32:35 -0400

    OCP break

--- Examples/SRP/src/OCP.java
+++ Examples/SRP/src/OCP.java
@@ -45,6 +45,14 @@ class ProductFiler {
 
 class Demo1 {
     public static void main(String[] args) {
+        Product apple = new Product("Apple", Color.GREEN, Size.SMALL);
+        Product tree = new Product("Tree", Color.GREEN, Size.LARGE);
+        Product house = new Product("House", Color.BLUE, Size.LARGE);
 
+        List<Product> products = List.of(apple, tree, house);
+
+        ProductFiler pf = new ProductFiler();
+        System.out.println("Green products (old): ");
+        pf.filterByColor(products, Color.GREEN).forEach(p -> System.out.println("- " + p.name + " is green"));
     }
 }
