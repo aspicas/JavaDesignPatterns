b8d08d1ea89bdb08856b5ac24c6328e00f9b2580
David Garcia <david.a.g.r93@gmail.com>
2018-09-18 23:05:36 -0400

    ISP break

--- Examples/SRP/src/ISP.java
+++ Examples/SRP/src/ISP.java
@@ -1,4 +1,46 @@
+class Document {
 
+}
+
+interface Machine {
+    void print(Document d);
+    void fax(Document d) throws Exception;
+    void scan(Document d);
+}
+
+class MultiFunctionPrinter implements Machine {
+    @Override
+    public void print(Document d) {
+        //
+    }
+
+    @Override
+    public void fax(Document d) throws Exception {
+        //
+    }
+
+    @Override
+    public void scan(Document d) {
+        //
+    }
+}
+
+class OldFashionedPrinter implements Machine {
+    @Override
+    public void print(Document d) {
+        //
+    }
+
+    @Override
+    public void fax(Document d) throws Exception {
+        throw new Exception();
+    }
+
+    @Override
+    public void scan(Document d) {
+        //
+    }
+}
 
 class Demo3 {
 
