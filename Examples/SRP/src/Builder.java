abe1b103ffa3d4e1e3d5bf8d06c65f5bbd32b361
David Garcia <david.a.g.r93@gmail.com>
2018-09-23 13:41:09 -0400

    When you need a builder

---
+++ Examples/SRP/src/Builder.java
@@ -0,0 +1,14 @@
+public class Builder {
+}
+
+class Demo5 {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        System.out.println("<p>" + hello + "</p>");
+
+        String [] words = {"hello", "world"};
+        System.out.println(
+                "<ul>\n" + "<li>" + words[0]
+        );
+    }
+}
