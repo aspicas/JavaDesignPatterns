2dc805c61d713118ddd6aca9516ba778b44d9438
David Garcia <david.a.g.r93@gmail.com>
2018-09-23 14:02:33 -0400

    Builder string

--- Examples/SRP/src/Builder.java
+++ Examples/SRP/src/Builder.java
@@ -1,14 +1,15 @@
-public class Builder {
-}
-
 class Demo5 {
     public static void main(String[] args) {
         String hello = "Hello";
         System.out.println("<p>" + hello + "</p>");
 
         String [] words = {"hello", "world"};
-        System.out.println(
-                "<ul>\n" + "<li>" + words[0]
-        );
+        StringBuilder sb = new StringBuilder();
+        sb.append("<ul>\n");
+        for (String word : words) {
+            sb.append(String.format(" <li>%s</li>\n", word));
+        }
+        sb.append("</ul>");
+        System.out.println(sb);
     }
 }
