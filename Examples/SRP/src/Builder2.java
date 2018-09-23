8d3debef51e2066cf75777a45ece45309cf54992
David Garcia <david.a.g.r93@gmail.com>
2018-09-23 19:25:13 -0400

    Fluent Builder Inheritance with Recursive Generics

--- Examples/SRP/src/Builder2.java
+++ Examples/SRP/src/Builder2.java
@@ -5,36 +5,49 @@ class Person2 {
     @Override
     public String toString() {
         return "Person{" +
-                "name=" + name + '\'' +
+                "name='" + name + '\'' +
                 ", position='" + position + '\'' +
                 '}';
     }
 }
 
-class PersonBuilder {
+class PersonBuilder<SELF extends PersonBuilder<SELF>> {
     protected Person2 person = new Person2();
 
-    public PersonBuilder withName(String name) {
+    public SELF withName(String name) {
         person.name = name;
-        return this;
+        return self();
     }
 
     public Person2 build() {
         return person;
     }
+
+    protected SELF self() {
+        return (SELF) this;
+    }
 }
 
 class EmployeeBuilder
-        extends PersonBuilder {
+        extends PersonBuilder<EmployeeBuilder> {
     public EmployeeBuilder worksAt(String position) {
         person.position = position;
+        return self();
+    }
+
+    @Override
+    protected EmployeeBuilder self() {
         return this;
     }
 }
 
 class Demo6 {
     public static void main(String[] args) {
-        PersonBuilder pb = new PersonBuilder();
-        Person2 david = pb.withName("David").build();
+        EmployeeBuilder pb = new EmployeeBuilder();
+        Person2 david = pb
+                .withName("David")
+                .worksAt("Developer")
+                .build();
+        System.out.println(david);
     }
 }
