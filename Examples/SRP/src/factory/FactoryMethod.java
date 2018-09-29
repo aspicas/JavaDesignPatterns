8ad33680da5d78bcc7a7357b2628670587212bae
David Garcia <david.a.g.r93@gmail.com>
2018-09-28 21:47:55 -0400

    Manage factory method

--- Examples/SRP/src/factory/FactoryMethod.java
+++ Examples/SRP/src/factory/FactoryMethod.java
@@ -1,30 +1,25 @@
 package factory;
 
-enum CoordinateSystem {
-    CARTESIAN,
-    POLAR
-}
-
 class Point {
     private double x, y;
 
-    /**
-     *
-     * @param a is x if cartisian or rho if polar
-     * @param b
-     * @param cs
-     */
-    public Point(double a, double b, CoordinateSystem cs) {
-        switch (cs){
-            case POLAR:
-                x = a * Math.cos(b);
-                y = a * Math.sin(b);
-                break;
-            case CARTESIAN:
-                this.x = a;
-                this.y = b;
-                break;
-        }
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point newCartesianPoint(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public static Point newPolarPoint(double rho, double theta) {
+        return new Point(rho * Math.cos(theta),
+                rho * Math.sin(theta));
     }
 }
 
+class Demo9 {
+    public static void main(String[] args) {
+        Point point = Point.newPolarPoint(2, 3);
+    }
+}
