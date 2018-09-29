da27f5e44e2cf0330ffd857e1d7661210d88e3f3
David Garcia <david.a.g.r93@gmail.com>
2018-09-28 22:37:06 -0400

    Factory class with private constructor

--- Examples/SRP/src/factory/FactoryMethod.java
+++ Examples/SRP/src/factory/FactoryMethod.java
@@ -3,26 +3,28 @@ package factory;
 class Point {
     private double x, y;
 
-    public Point(double x, double y) {
+    private Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
-}
 
-class PointFactory {
-    public static Point newCartesianPoint(double x, double y) {
-        return new Point(x, y);
+    public static class Factory {
+        public static Point newCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
+
+        public static Point newPolarPoint(double rho, double theta) {
+            return new Point(rho * Math.cos(theta),
+                    rho * Math.sin(theta));
+        }
     }
 
-    public static Point newPolarPoint(double rho, double theta) {
-        return new Point(rho * Math.cos(theta),
-                rho * Math.sin(theta));
-    }
 }
+
+
 
 class Demo9 {
     public static void main(String[] args) {
-        Point point = PointFactory.newCartesianPoint(2, 3);
-        Point point1 = new Point(2, 3);
+        Point point = Point.Factory.newCartesianPoint(2, 3);
     }
 }
