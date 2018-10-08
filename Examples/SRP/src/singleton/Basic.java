e61cc2e16f1a0707d0fadc58deacd3f9c77e4f7b
David Garcia <david.a.g.r93@gmail.com>
2018-10-07 20:35:05 -0400

    Serialization problem

--- Examples/SRP/src/singleton/Basic.java
+++ Examples/SRP/src/singleton/Basic.java
@@ -1,6 +1,8 @@
 package singleton;
 
-class BasicSingleton {
+import java.io.*;
+
+class BasicSingleton implements Serializable {
 
     private BasicSingleton() {
 
@@ -24,14 +26,39 @@ class BasicSingleton {
 }
 
 class Demo15 {
-    public static void main(String[] args) {
-        BasicSingleton singleton = BasicSingleton.getInstance();
-        BasicSingleton singleton2 = BasicSingleton.getInstance();
-        singleton.setValue(123);
-        System.out.println(singleton.getValue());
 
-        singleton2.setValue(1234);
-        System.out.println(singleton2.getValue());
+    static void saveToFile(BasicSingleton singleton,
+                           String filename) throws Exception {
+        try (FileOutputStream fileOut = new FileOutputStream(filename);
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(singleton);
+        }
+    }
+
+    static BasicSingleton readFromFile(String filename)
+            throws Exception {
+        try (FileInputStream fileIn = new FileInputStream(filename);
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            return (BasicSingleton) in.readObject();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 1. Reflection
+        // 2. Serialization
+
+        BasicSingleton singleton = BasicSingleton.getInstance();
+        singleton.setValue(111);
+
+        String filename = "singleton.bin";
+        saveToFile(singleton, filename);
+
+        singleton.setValue(222);
+
+        BasicSingleton singleton2 = readFromFile(filename);
+
+        System.out.println(singleton == singleton2);
         System.out.println(singleton.getValue());
+        System.out.println(singleton2.getValue());
     }
 }
