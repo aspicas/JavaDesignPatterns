8299ea0507f0739784cd9abe8f2cbb655a3ad18f
David Garcia <david.a.g.r93@gmail.com>
2018-10-27 17:24:19 -0400

    Needed the adapter

---
+++ Examples/SRP/src/structural/adapter/Adapter.java
@@ -0,0 +1,64 @@
+package structural.adapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Point {
+
+    public int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+}
+
+class Line {
+
+    public Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+}
+
+class VectorObject extends ArrayList<Line> {
+
+}
+
+class VectorRectangle extends VectorObject {
+    public VectorRectangle(int x, int y, int width, int height) {
+        add(new Line(new Point(x, y), new Point(x + width, y)));
+        add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+        add(new Line(new Point(x, y), new Point(x + width, y + height)));
+        add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+    }
+}
+
+class Demo18 {
+    private final static List<VectorObject> vectorObjects = new ArrayList<>(Arrays.asList(
+            new VectorRectangle(1, 1, 10, 10),
+            new VectorRectangle(3, 3, 6, 6)
+    ));
+
+    public static void drawPoint(Point p) {
+        System.out.println(".");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
