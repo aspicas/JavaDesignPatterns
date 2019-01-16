235654f4ff6acd8f050baf5dc933ee7d90f2fe1a
David Garcia <david.a.g.r93@gmail.com>
2019-01-16 22:54:14 +0100

    With compose, you can take a singular (scalar) element and you can still implement iterable

--- Examples/SRP/src/structural/composite/Composite2.java
+++ Examples/SRP/src/structural/composite/Composite2.java
@@ -1,17 +1,43 @@
 package structural.composite;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
-class Neuron {
-    public ArrayList<Neuron> in, out;
-
-//    public void connectTo(Neuron other) {
-//        out.add(other);
-//        other.in.add(this);
-//    }
+interface SomeNeurons extends Iterable<Neuron> {
+    default void connectTo(SomeNeurons other) {
+        if (this == other) return;
+        for (Neuron from : this) {
+            for (Neuron to : other) {
+                from.out.add(to);
+                to.in.add(from);
+            }
+        }
+    }
 }
 
-class NeuronLayer extends ArrayList<Neuron> {
+class Neuron implements SomeNeurons {
+    public ArrayList<Neuron> in, out;
+
+    @Override
+    public void forEach(Consumer<? super Neuron> action) {
+        action.accept(this);
+    }
+
+    @Override
+    public Spliterator<Neuron> spliterator() {
+        return Collections.singleton(this).spliterator();
+    }
+
+    @Override
+    public Iterator<Neuron> iterator() {
+        return Collections.singleton(this).iterator();
+    }
+}
+
+class NeuronLayer extends ArrayList<Neuron> implements SomeNeurons {
 
 }
 
