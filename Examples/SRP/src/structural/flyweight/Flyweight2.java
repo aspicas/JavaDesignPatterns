d64e216cc78b52560ecfab0b9576b02f83384bc7
David Garcia <david.a.g.r93@gmail.com>
2019-01-31 19:53:53 +0100

    Flyweight implemented

--- Examples/SRP/src/structural/flyweight/Flyweight2.java
+++ Examples/SRP/src/structural/flyweight/Flyweight2.java
@@ -1,5 +1,9 @@
 package structural.flyweight;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//Without Pattern
 class FormattedTex {
     private String plainText;
     private boolean [] capitalize;
@@ -27,10 +31,59 @@ class FormattedTex {
     }
 }
 
+//With Pattern
+class BetterFormattedText {
+    private String plainText;
+    private List<TextRange> formatting = new ArrayList<>();
+
+    public BetterFormattedText(String plainText) {
+        this.plainText = plainText;
+    }
+
+    public TextRange getRange(int start, int end) {
+        TextRange range = new TextRange(start, end);
+        formatting.add(range);
+        return range;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < plainText.length(); ++i) {
+            char c = plainText.charAt(i);
+            for (TextRange range : formatting) {
+                if (range.covers(i) && range.capitalize) {
+                    c = Character.toUpperCase(c);
+                }
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public class TextRange { //Flyweight
+        public int start, end;
+        public boolean capitalize, bold, italic;
+
+        public TextRange(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public boolean covers(int position) {
+            return position >= start && position <= end;
+        }
+    }
+}
+
 class Demo27 {
     public static void main(String[] args) {
         FormattedTex ft = new FormattedTex("This is a brave new world");
         ft.capitalize(10, 15);
         System.out.println(ft.toString());
+
+        BetterFormattedText bft = new BetterFormattedText("Make America Great Again");
+        bft.getRange(13, 18).capitalize = true;
+        System.out.println(bft.toString());
     }
 }
