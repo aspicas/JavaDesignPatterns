197c9336c251640d2ba2313615606a27d74df95c
David Garcia <david.a.g.r93@gmail.com>
2019-01-31 16:49:42 +0100

    Problematic because we are allocating a boolean for every single value

--- Examples/SRP/src/structural/flyweight/Flyweight2.java
+++ Examples/SRP/src/structural/flyweight/Flyweight2.java
@@ -1,4 +1,36 @@
 package structural.flyweight;
 
-public class Flyweight2 {
+class FormattedTex {
+    private String plainText;
+    private boolean [] capitalize;
+
+    public FormattedTex(String plainText) {
+        this.plainText = plainText;
+        capitalize = new boolean[plainText.length()];
+    }
+
+    public void capitalize(int start, int end) {
+        for (int i = start; i <= end; ++i)
+            capitalize[i] = true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < plainText.length(); ++i) {
+            char c = plainText.charAt(i);
+            sb.append(
+                    capitalize[i] ? Character.toUpperCase(c) : c
+            );
+        }
+        return sb.toString();
+    }
+}
+
+class Demo27 {
+    public static void main(String[] args) {
+        FormattedTex ft = new FormattedTex("This is a brave new world");
+        ft.capitalize(10, 15);
+        System.out.println(ft.toString());
+    }
 }
