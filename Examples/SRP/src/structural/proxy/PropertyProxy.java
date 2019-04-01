ba8320afb0b644d3405ef40214d598b26443b2f0
David Garcia <david.a.g.r93@gmail.com>
2019-04-01 13:01:06 +0200

    Bad property status

---
+++ Examples/SRP/src/structural/proxy/PropertyProxy.java
@@ -0,0 +1,19 @@
+package structural.proxy;
+
+class Creature {
+    private int agility;
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+}
+
+class Demo30 {
+    public static void main(String[] args) {
+
+    }
+}
