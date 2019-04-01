458504c5ac1f79c1696b04f30cf7700f7560464d
David Garcia <david.a.g.r93@gmail.com>
2019-04-01 13:17:06 +0200

    Property proxy give you more control over the fields

--- Examples/SRP/src/structural/proxy/PropertyProxy.java
+++ Examples/SRP/src/structural/proxy/PropertyProxy.java
@@ -1,19 +1,56 @@
 package structural.proxy;
 
-class Creature {
-    private int agility;
+class Property<T> {
+    private T value;
 
-    public int getAgility() {
-        return agility;
+    public Property(T value) {
+        this.value = value;
     }
 
-    public void setAgility(int agility) {
-        this.agility = agility;
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
+class Creature {
+    private Property<Integer> agility = new Property<>(10);
+
+    public void setAgility(int value) {
+        agility.setValue(value);
+    }
+
+    public int getAgility() {
+        return agility.getValue();
     }
 }
 
 class Demo30 {
     public static void main(String[] args) {
-
+        Creature creature = new Creature();
+        creature.setAgility(10);
     }
 }
